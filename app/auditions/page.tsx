e0c33ba896efb12553f593a836b6327e7071499a
Arshil Yusuf <arshilyusuf@gmail.com>
2026-08-31 22:58:05 +0530

    Update page.tsx to only allow registrations up to 3rd year

--- app/auditions/page.tsx
+++ app/auditions/page.tsx
@@ -369,8 +369,6 @@ export default function Page() {
                     <option value="1">1st Year</option>
                     <option value="2">2nd Year</option>
                     <option value="3">3rd Year</option>
-                    <option value="4">4th Year</option>
-                    <option value="5">5th Year (B. Arch.)</option>
                   </select>
                 </div>
 
